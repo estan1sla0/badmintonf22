4f414022b78f51694a701a2b14c4b6c14f618a4c
estan1sla0 <estanislaoothar@live.com.ar>
2025-04-24 22:06:21 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,21 @@
-// CONFIGURACIÓN FIREBASE (reemplazá con tu propia config de Firebase)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_AUTH_DOMAIN",
-    projectId: "TU_PROJECT_ID",
-    storageBucket: "TU_STORAGE_BUCKET",
-    messagingSenderId: "TU_SENDER_ID",
-    appId: "TU_APP_ID"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-  
+  apiKey: "AIzaSyC6nR4ZofWhRSgBLT9E4Q5SfS7yG7stP5k",
+  authDomain: "badmintonwebtraining.firebaseapp.com",
+  projectId: "badmintonwebtraining",
+  storageBucket: "badmintonwebtraining.firebasestorage.app",
+  messagingSenderId: "787662262814",
+  appId: "1:787662262814:web:12dd855ffbe39942e1940f",
+  measurementId: "G-2YSZNYYEN8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
